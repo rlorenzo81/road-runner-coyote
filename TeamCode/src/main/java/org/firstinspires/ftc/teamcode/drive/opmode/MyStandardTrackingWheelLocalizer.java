57c0a32579fe870d93b8c9e8cd17ebafa16888f4
Robert Lorenzo <rlorenzo@essextech.org>
2020-03-03 11:51:30 -0500

    Update MyStandardTrackingWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MyStandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MyStandardTrackingWheelLocalizer.java
@@ -6,6 +6,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.RobotLog;
 
@@ -27,25 +28,30 @@ import java.util.List;
  */
 @Config
 public class MyStandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
-    public static double TICKS_PER_REV = 3100;//3806
-    public static double WHEEL_RADIUS = 1.496; // in
+    public static double TICKS_PER_REV = 1450;//3806
+    public static double WHEEL_RADIUS = 0.748031; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 15.7; // in; distance between the left and right wheels 15.7
-    public static double FORWARD_OFFSET = 5.5; // in; offset of the lateral wheel 5.5
+    public static double LATERAL_DISTANCE = 15.35; // in; distance between the left and right wheels 15.5
+    public static double FORWARD_OFFSET = -5.5; // in; offset of the lateral wheel -6.25
 
     private DcMotor leftEncoder, rightEncoder, frontEncoder;
 
     public MyStandardTrackingWheelLocalizer(HardwareMap hardwareMap) {
         super(Arrays.asList(
-                new Pose2d(2, LATERAL_DISTANCE / 2, 0), // left
-                new Pose2d(2, -LATERAL_DISTANCE / 2, 0), // right
-                new Pose2d(FORWARD_OFFSET, 2, Math.toRadians(90)) // front
+                new Pose2d(-1.3125, LATERAL_DISTANCE / 2, 0), // left
+                new Pose2d(-1.3125, -LATERAL_DISTANCE / 2, 0), // right
+                new Pose2d(FORWARD_OFFSET, 0.875, Math.toRadians(90)) // front
         ));
 
         leftEncoder = hardwareMap.dcMotor.get("leftEncoder");
         rightEncoder = hardwareMap.dcMotor.get("rightEncoder");
         frontEncoder = hardwareMap.dcMotor.get("frontEncoder");
+
+
+        rightEncoder.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftEncoder.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontEncoder.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public static double encoderTicksToInches(int ticks) {
