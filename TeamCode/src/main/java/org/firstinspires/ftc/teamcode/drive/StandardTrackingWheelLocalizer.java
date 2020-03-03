b77317c43f0b46eb45eb75e097c0eee588aebce5
Robert Lorenzo <rlorenzo@essextech.org>
2020-03-03 11:51:16 -0500

    Update StandardTrackingWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -7,6 +7,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.util.Arrays;
@@ -32,7 +33,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double LATERAL_DISTANCE = 15.7; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 5.5; // in; offset of the lateral wheel
+    public static double FORWARD_OFFSET = -6.75; // in; offset of the lateral wheel 5.5
 
     private DcMotor leftEncoder, rightEncoder, frontEncoder;
 
@@ -46,6 +47,10 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         leftEncoder = hardwareMap.dcMotor.get("leftEncoder");
         rightEncoder = hardwareMap.dcMotor.get("rightEncoder");
         frontEncoder = hardwareMap.dcMotor.get("frontEncoder");
+
+        rightEncoder.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftEncoder.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontEncoder.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public static double encoderTicksToInches(int ticks) {
